d66580f8069b2dba7f76dcaf1606205e787a8042
German Izhboldin <w1ss@yandex.ru>
2023-05-04 13:43:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int sum = 20;
+
+        int miles = sum / 20;
+
+        System.out.println("Итоговый бонус:" + miles);
+
+    }
+}
